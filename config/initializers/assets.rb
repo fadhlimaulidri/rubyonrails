0935e7f125f56850c91e43bf93ee74f16cc51695
Fadhli Maulidri <fadhlimaulidri@gmail.com>
2019-06-07 07:19:46 +0700

    deploy

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,20 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( fontawesome-webfont.eot fontawesome-webfont.svg fontawesome-webfont.ttf fontawesome-webfont.woff fontawesome-webfont.woff2 FontAwesome.otf Simple-Line-Icons.dev.svg Simple-Line-Icons.eot Simple-Line-Icons.svg Simple-Line-Icons.ttf Simple-Line-Icons.woff Simple-Line-Icons.woff2 weathericons-regular-webfont.eot weathericons-regular-webfont.svg weathericons-regular-webfont.ttf weathericons-regular-webfont.woff weathericons-regular-webfont.woff2 jquery.js material/moment.js bootstrap.js application.css bootstrap.css font-awesome.css home.scss lineicons.css mystyles.css weather-icons.css)
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.eot fontawesome-webfont.svg fontawesome-webfont.ttf fontawesome-webfont.woff fontawesome-webfont.woff2 FontAwesome.otf Simple-Line-Icons.dev.svg Simple-Line-Icons.eot Simple-Line-Icons.svg Simple-Line-Icons.ttf Simple-Line-Icons.woff Simple-Line-Icons.woff2 weathericons-regular-webfont.eot weathericons-regular-webfont.svg weathericons-regular-webfont.ttf weathericons-regular-webfont.woff weathericons-regular-webfont.woff2)
+
+Rails.application.config.assets.precompile += %w( material/bootstrap.css)
+Rails.application.config.assets.precompile += %w( material/application.css)
+Rails.application.config.assets.precompile += %w( material/bootstrap.css)
+Rails.application.config.assets.precompile += %w( material/font-awesome.css)
+Rails.application.config.assets.precompile += %w( material/home.css)
+Rails.application.config.assets.precompile += %w( material/lineicons.css)
+Rails.application.config.assets.precompile += %w( material/mystyles.css)
+Rails.application.config.assets.precompile += %w( material/weather-icons.css)
+
+Rails.application.config.assets.precompile += %w( material/jquery.js)
+Rails.application.config.assets.precompile += %w( material/moment.js)
+Rails.application.config.assets.precompile += %w( material/bootstrap.js)
 Rails.application.config.assets.precompile += %w( material/owl-carousel.js)
 Rails.application.config.assets.precompile += %w( material/blur-area.js)
 Rails.application.config.assets.precompile += %w( material/icheck.js)
