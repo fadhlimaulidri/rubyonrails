05b2a2e0ab442ac2e6a701680e0a8fce14784682
Fadhli Maulidri <fadhlimaulidri@gmail.com>
2019-06-08 07:23:08 +0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# user = User.create(
-#   {username: 'fadhlimaulidri', name: 'Fadhli Maulidri', provider: 'github'}
-# )
-#
-# Trip.create([
-#     {user_id: 1, dep: 'Padang', arr: 'Bukittinggi', dep_time: '2019-09-10 00:00:00.000000000 +0000', user_transport_id: 1, fare: 30000},
-#     {user_id: 1, dep: 'Pekanbaru', arr: 'Bukittinggi', dep_time: '2019-09-10 00:00:00.000000000 +0000', user_transport_id: 1, fare: 50000},
-#     {user_id: 1, dep: 'Payakumbuh', arr: 'Bukittinggi', dep_time: '2019-09-10 00:00:00.000000000 +0000', user_transport_id: 1, fare: 20000}
-# ])
+user = User.create(
+  {username: 'fadhlimaulidri', name: 'Fadhli Maulidri', provider: 'github'}
+)
+
+Trip.create([
+    {user_id: 1, dep: 'Padang', arr: 'Bukittinggi', dep_time: '2019-09-10 00:00:00.000000000 +0000', user_transport_id: 1, fare: 30000},
+    {user_id: 1, dep: 'Pekanbaru', arr: 'Bukittinggi', dep_time: '2019-09-10 00:00:00.000000000 +0000', user_transport_id: 1, fare: 50000},
+    {user_id: 1, dep: 'Payakumbuh', arr: 'Bukittinggi', dep_time: '2019-09-10 00:00:00.000000000 +0000', user_transport_id: 1, fare: 20000}
+])
 
 ToggleFeature.create([
   {feature_name: 'comming soon', state: true}
